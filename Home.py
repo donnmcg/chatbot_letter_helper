e156f1d4f5d53a4aabbe3ca3a498b46e8c86ee71
Donnacha <donnacha.mcgrath4@gmail.com>
2023-05-02 17:44:53 +1000

    Add spinners. Add comments. Update chat function. Add help icons.

--- Home.py
+++ Home.py
@@ -1,4 +1,3 @@
-import os
 import openai
 import streamlit as st
 
@@ -7,24 +6,25 @@ API_KEY = st.secrets["API_KEY"]
 openai.api_key = API_KEY
 
 
-def chat_practice(question):
-    if question == "exit":
-        return "exit"
-    else:
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "user", "content": question}
-            ]
-        )
-        return completion
+def send_chat_request(question):
+    """
+    :param question: should be a str in the form of a question.
+    :return: a str response from ChatGPT.
+    """
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": question}
+        ]
+    )
+    return completion
 
 
+# Set up the page
 st.title("Letter to the Editor")
 st.subheader("Use ChatGPT to help you write a Letter to the Editor")
 st.write('Fill in the boxes below, enter the password and press "Ask".')
 st.write("It isn't necessary to answer every question.")
-# st.write("But you need to provide information on the issue or subject.")
 st.write("Note: Do not add personal information "
          "like your name, address and email into the form below.")
 
@@ -33,10 +33,15 @@ article_title = st.text_input("What is the article title?",
 article_author = st.text_input("Who is the author?",
                                placeholder="Author")
 pub_date = st.text_input("What date was the article published on?",
-                         placeholder="Date")
-support = st.text_input("Do you support the views in the article?")
+                         placeholder="DD/MM/YYYY")
+support = st.text_input("Do you support the views in the article?",
+                        help="This can be a yes or no answer. Or you can write"
+                             " about how you support some points and not "
+                             "others.")
 topic = st.text_area("What is the issue or subject you want to address "
-                     "in your letter to the editor?")
+                     "in your letter to the editor?",
+                     help="Describe what the article is about and what "
+                          "your own thoughts are on the subject.")
 tone = st.text_input("What is the tone of your letter? "
                      "Is it persuasive, informative, argumentative, etc.?",
                      placeholder="Persuasive")
@@ -45,7 +50,9 @@ length = st.text_input("How long should the article be?",
 personal_experience = st.text_area("Do you have any personal experience"
                                    " related to the topic?")
 evidence = st.text_area("Do you have any evidence or statistics to support"
-                        " your argument?")
+                        " your argument?",
+                        help="This could be links to articles or "
+                             "research papers. Or some other information.")
 call_to_action = st.text_area("What action do you want the readers or the "
                               "publication to take after reading your letter?")
 password = st.text_input("Password: ", type="password")
@@ -62,9 +69,10 @@ if not length:
 if not tone:
     tone = "persuasive"
 
+# Create the query that will be sent to ChatGPT
 query_combined = f"""
 Please write a letter to the editor that includes the following items:
-The topic: {topic}
+The topic that should be discussed: {topic}
 Do I support the article: {support}
 The response is to this article: 
     Title: {article_title} 
@@ -75,18 +83,26 @@ Try to keep it short. My preferred article length in words is: {length}
 Evidence that could be used: {evidence}
 The call to action I'd like to appear: {call_to_action}
 """
+
 # When button pressed:
 if ask_button:
     # Only run the query if the password is correct
     if password == st.secrets["PASSWORD"]:
-        moderation_response = openai.Moderation.create(
-            input=query_combined
-        )
+        # Show a message to show that the request is being made.
+        with st.spinner(text="Please wait..."):
+            # Perform a moderation check.
+            moderation_response = openai.Moderation.create(
+                input=query_combined
+            )
         output = moderation_response['results'][0]['flagged']
+        # If moderation ok (False). Then put the request through.
         if output is False:
-            response = chat_practice(str(query_combined))
+            # Let the user know there is something happening with a spinner.
+            with st.spinner(text="Please wait..."):
+                response = send_chat_request(str(query_combined))
             st.write(response["choices"][0]["message"]["content"])
-            st.write(response["usage"]["total_tokens"])
+            # How many tokens were used:
+            # st.write(response["usage"]["total_tokens"])
         else:
             st.info("Your request has been flagged as against usage policies. "
                     "Please revise your answers and try again.")
