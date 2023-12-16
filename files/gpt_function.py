2f2a29f71349111b3b6d9dd90e4e83e630c034cd
Donnacha <donnacha.mcgrath4@gmail.com>
2023-12-16 17:47:17 +1100

    Add temperature.

--- files/gpt_function.py
+++ files/gpt_function.py
@@ -12,10 +12,13 @@ def send_chat_request(question):
     """
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
+        # model="gpt-3.5-turbo-instruct",
         # model="gpt-3.5-turbo-16k",
         messages=[
             {"role": "user", "content": question}
-        ]
+        ],
+        temperature=0.9,
+        # top_p=0.9
     )
     return completion
 
